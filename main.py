6d6f726335a4ada342cbe64e4996793d700b198f
praveensampath9 <101631187+praveensampath9@users.noreply.github.com>
2023-01-26 19:06:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #Write your code here.
+try:
+    price = float(input())
+    vat = float(input())
+    vat_amount = price * (vat/100)
+    total_price = price + vat_amount
+    print(round(total_price))
+except:
+    print("Invalid Input")
